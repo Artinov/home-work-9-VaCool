fcbaca43869790cac41b47d5f7d01cf546dad5b1
VaCool <18rammzes@gmail.com>
2017-03-15 19:08:16 +0200

    оптимизировал

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,7 @@ var ball = document.querySelector(".discoball");
 var	elements = "here goes our awesome disco ball".split(" ");
 var	i = 0;
 function goDisco() {
-	alert(elements[1]);
 		ball.setAttribute("class", "discoball " + elements[i]);
-		i++;
-		(i > 5) ? i = 0: i = i;
+		(i > 5) ? i = 0: i++;;
 	}
 setInterval(goDisco,300);
